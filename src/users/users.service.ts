b69dc4aaa560bc823dc2812e5f298fbf723c41e0
Saif Ali <saifali@truemedit.com>
2025-06-28 00:21:45 +0500

    feat: add TypeORM integration and nodemon for development
    - Implemented TypeORM for PostgreSQL database connection in app.module.ts
    - Created User entity with necessary fields in user.entity.ts
    - Updated UsersModule to include TypeORM features
    - Refactored UsersService to utilize TypeORM for user management
    - Modified start scripts in package.json to use nodemon for development
    - Added nodemon configuration file for better development experience

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,34 @@
+// src/user/user.service.ts
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private userRepo: Repository<User>,
+  ) {}
+
+  create(data: Partial<User>) {
+    const user = this.userRepo.create(data);
+    return this.userRepo.save(user);
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.userRepo.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.userRepo.findOneBy({ id });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: number, data: Partial<User>) {
+    return this.userRepo.update(id, data);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  delete(id: number) {
+    return this.userRepo.delete(id);
   }
 }
